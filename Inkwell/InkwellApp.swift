d7707521043edbe07fbdd27cf8905b7248014eb0
Ewan Croft <git@ewancroft.uk>
2026-06-19 01:52:03 +0100

    Initial Commit

---
+++ Inkwell/InkwellApp.swift
@@ -0,0 +1,32 @@
+//
+//  InkwellApp.swift
+//  Inkwell
+//
+//  Created by Ewan Croft on 19/06/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct InkwellApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
