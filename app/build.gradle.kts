ec41b83e2ed8069be87a29a86d5c605a4ca8e664
Ewan Croft <contact@ewancroft.uk>
2026-08-16 00:25:19 +0100

    chore: version bump to 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "uk.ewancroft.inkwell"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.1.0"
 
         manifestPlaceholders["appAuthRedirectScheme"] = "uk.ewancroft.inkwell"
     }
